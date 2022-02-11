491138c83452547c4b3fac25c183176a3b5e7c8d
Tomasz Łakota <tlakota@kplabs.pl>
2022-02-11 13:41:58 +0100

    Typing with mypy.

--- progress_checkpoint/asyn.py
+++ progress_checkpoint/asyn.py
@@ -1,9 +1,11 @@
-from typing import Sequence, Iterable
+from typing import Sequence, Iterable, AsyncGenerator, Optional
 
-from .common import AsyncCheckpoint
+from .common import AsyncCheckpoint, StatusMessage, ProgressFraction
 
 
-async def with_progress_async(seq: Iterable, checkpoint: AsyncCheckpoint, size=None, status=None, div=1):
+async def with_progress_async(seq: Iterable, checkpoint: AsyncCheckpoint, size: Optional[int] = None,
+                              status: Optional[StatusMessage] = None, div: int = 1
+                              ) -> AsyncGenerator[AsyncCheckpoint, None]:
     await checkpoint(0, status or '')
     if size is None:
         if not isinstance(seq, Sequence):
@@ -18,5 +20,6 @@ async def with_progress_async(seq: Iterable, checkpoint: AsyncCheckpoint, size=N
 
 
 # noinspection PyUnusedLocal
-async def dummy_checkpoint_async(progress, status=None):
-    return
+async def dummy_checkpoint_async(progress: ProgressFraction, status: Optional[StatusMessage] = None
+                                 ) -> AsyncGenerator[None, None]:
+    pass
